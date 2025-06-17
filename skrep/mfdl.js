b0ef419008e70341ff91ce8e510a42a69a51a633
Sxyz <suzakuteamhere@gmail.com>
2025-06-17 19:25:53 +0700

    Update mfdl.js

--- skrep/mfdl.js
+++ skrep/mfdl.js
@@ -2,22 +2,24 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 
 export default async function mfdl(url) {
- try {
-  const { data } = await axios.get(url);
-  const $ = cheerio.load(data);
+  try {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
 
-  const downloadElement = $('#downloadButton');
+    const downloadElement = $("#downloadButton");
 
-  const downloadUrl = downloadElement.attr('href');
-  const fileName = $('.dl-btn-label').text().trim();
-  const fileSize = downloadElement.text().trim();
+    const downloadUrl = downloadElement.attr("href");
+    const fileName = $(".dl-btn-label").text().trim();
+    const fileSize = downloadElement.text().trim();
 
-  if (!downloadUrl) throw new Error("Gagal menemukan link download.");
+    if (!downloadUrl) throw new Error("Gagal menemukan link download.");
 
-  return {
-    fileName,
-    fileSize,
-    downloadUrl
-  };
- } catch (e) { return Promise.reject(e) }
+    return {
+      fileName,
+      fileSize,
+      downloadUrl,
+    };
+  } catch (e) {
+    return Promise.reject(e);
+  }
 }
