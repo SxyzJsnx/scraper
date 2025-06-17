16efc5b977d1b2a91392c70e71d470c95c63a5a6
Sxyz <suzakuteamhere@gmail.com>
2025-06-17 19:23:56 +0700

    Create mfdl.js

---
+++ skrep/mfdl.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+export default async function mfdl(url) {
+ try {
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+
+  const downloadElement = $('#downloadButton');
+
+  const downloadUrl = downloadElement.attr('href');
+  const fileName = $('.dl-btn-label').text().trim();
+  const fileSize = downloadElement.text().trim();
+
+  if (!downloadUrl) throw new Error("Gagal menemukan link download.");
+
+  return {
+    fileName,
+    fileSize,
+    downloadUrl
+  };
+ } catch (e) { return Promise.reject(e) }
+}
